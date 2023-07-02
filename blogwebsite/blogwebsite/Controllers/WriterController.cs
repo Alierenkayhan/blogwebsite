7ab634a0734f6a95e20a0109b5e3b2751e56aa91
Ali Eren Kayhan <ali_erenbatman@hotmail.com>
2023-07-02 21:15:27 +0300

    Starting project

---
+++ blogwebsite/blogwebsite/Controllers/WriterController.cs
@@ -0,0 +1,75 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blogwebsite.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterDal());
+        WriterValidator validationRules = new WriterValidator();
+
+        public ActionResult Index()
+        {
+            var WriterValue = wm.GetList();
+            return View(WriterValue);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            ValidationResult results = validationRules.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditWriter(int id) 
+        { 
+            var writervalue = wm.GetByID(id);
+            return View(writervalue);
+        }
+        [HttpPost]
+        public ActionResult EditWriter(Writer writer)
+        {
+            ValidationResult results = validationRules.Validate(writer);
+            if (results.IsValid)
+            {
+                wm.WriterUpdate(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
