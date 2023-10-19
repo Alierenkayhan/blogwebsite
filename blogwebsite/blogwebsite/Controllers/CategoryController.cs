e5abc57adadfae69bca8137ae0524f67d275bf4e
Ali Eren Kayhan <ali_erenbatman@hotmail.com>
2023-10-19 18:26:54 +0300

    Project Init

---
+++ blogwebsite/blogwebsite/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
+using FluentValidation.Results;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blogwebsite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        // GET: Category
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            FluentValidation.Results.ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ blogwebsite/blogwebsite/Controllers/HeadingController.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blogwebsite.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        WriterManager wm = new WriterManager(new EFWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            //Linq olarak category leri list olarak alıyoruz
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }
+                                                  ).ToList();
+
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurName,
+                                                    Value = x.WriterID.ToString()
+                                                }
+                                                  ).ToList();
+            ViewBag.vlw = valuewriter;
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }
+                                                  ).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValues = hm.GetByID(id);
+            return View(HeadingValues);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            HeadingValue.HeadingStatus = false;
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("Index");
+        }
+    }
+}
